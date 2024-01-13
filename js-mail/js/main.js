8bfdf0f2ee4f1ed0b4c4ebdb185d756cc125f8b2
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-01-13 10:18:09 +0100

    feat add input inserted checker

--- js-mail/js/main.js
+++ js-mail/js/main.js
@@ -14,19 +14,30 @@ const registerButton = document.getElementById("register");
 // Creo l'event listener sul buttone
 sendButton.addEventListener("click", function () {
   const insertedMail = userInput.value;
-
   let resultText = "ACCESSO NEGATO - LA TUA MAIL NON E' PRESENTE";
-  // controllo presenza mail all'interno dell'array
-  for (let i = 0; i < mailList.length; i++) {
-    if (insertedMail === mailList[i]) {
-      resultText = "BENVENUTO";
-      break;
-    }
-  }
 
-  if (resultText !== "BENVENUTO") {
-    registerButton.classList.remove("d-none");
-    registerButton.classList.add("d-block");
+  if (insertedMail) {
+    // controllo presenza mail all'interno dell'array
+    for (let i = 0; i < mailList.length && resultText !== "BENVENUTO"; i++) {
+      if (insertedMail === mailList[i]) {
+        resultText = "BENVENUTO";
+      }
+      console.log(i);
+      registerButton.classList.add("d-none");
+      registerButton.classList.remove("d-block");
+    }
+
+    if (resultText !== "BENVENUTO") {
+      registerButton.classList.remove("d-none");
+      registerButton.classList.add("d-block");
+    }
+
+    checkResult.classList.remove("text-danger");
+    userInput.classList.remove("input-error");
+  } else {
+    checkResult.classList.add("text-danger");
+    userInput.classList.add("input-error");
+    resultText = "Errore - Si prega di inserire una mail corretta";
   }
   // stampo sulla pagina il risultato
   checkResult.innerText = resultText;
@@ -35,6 +46,7 @@ sendButton.addEventListener("click", function () {
 registerButton.addEventListener("click", function () {
   const insertedMail = userInput.value;
   mailList.push(insertedMail);
+  userInput.value = "";
   checkResult.innerText = "Ti sei registrato correttamente";
   console.log(mailList);
   registerButton.classList.remove("d-block");
