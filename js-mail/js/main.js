44c5bd2e7fb8b38f3b0b731c775b8d7a8139c58c
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-05-30 22:35:00 +0200

    fix: js-mail refactoring

--- js-mail/js/main.js
+++ js-mail/js/main.js
@@ -1,5 +1,4 @@
-// dichiaro il mio array database
-const mailList = [
+const mails = [
   "vincenzo.mercadante0@outlook.it",
   "pinco.pallo@user.it",
   "ziopeppo@ziappeppa.it",
@@ -8,47 +7,81 @@ const mailList = [
 // inizializzo le costanti degli elementi dell'HTML
 const userInput = document.getElementById("user-mail");
 const sendButton = document.getElementById("submit-btn");
-const checkResult = document.getElementById("result");
+const checkResultContainer = document.getElementById("result");
 const registerButton = document.getElementById("register");
 
-// Creo l'event listener sul buttone
+// event that starts the mail verification
 sendButton.addEventListener("click", function () {
   const insertedMail = userInput.value;
   let resultText = "ACCESSO NEGATO - LA TUA MAIL NON E' PRESENTE";
-
+  
   if (insertedMail) {
-    // controllo presenza mail all'interno dell'array
-    for (let i = 0; i < mailList.length && resultText !== "BENVENUTO"; i++) {
-      if (insertedMail === mailList[i]) {
-        resultText = "BENVENUTO";
-      }
-      console.log(i);
-      registerButton.classList.add("d-none");
-      registerButton.classList.remove("d-block");
+    removeErrorStyle()
+    
+    // check if the mail is include in array
+    const insertedMailInArray = userInputInArray(insertedMail)
+
+    // changing result based on the result
+    if (insertedMailInArray) {
+      resultText = "BENVENUTO";
+    } else {
+      buttonToggleVisibility()
     }
 
-    if (resultText !== "BENVENUTO") {
-      registerButton.classList.remove("d-none");
-      registerButton.classList.add("d-block");
-    }
-
-    checkResult.classList.remove("text-danger");
-    userInput.classList.remove("input-error");
   } else {
-    checkResult.classList.add("text-danger");
-    userInput.classList.add("input-error");
+    addErrorStyle()
     resultText = "Errore - Si prega di inserire una mail corretta";
   }
-  // stampo sulla pagina il risultato
+  // print the result on page
   checkResult.innerText = resultText;
 });
 
+// event handling for the register button which add mail in the array
 registerButton.addEventListener("click", function () {
+  // get the mail from the input
   const insertedMail = userInput.value;
-  mailList.push(insertedMail);
+  // clear the input field
   userInput.value = "";
+  // add mail in the array
+  mails.push(insertedMail);
+  // change result text
   checkResult.innerText = "Ti sei registrato correttamente";
-  console.log(mailList);
-  registerButton.classList.remove("d-block");
-  registerButton.classList.add("d-none");
+  buttonToggleVisibility()
 });
+
+/**
+ * 
+ * @param {string} userMail represent the mail inserted by the user
+ * @returns true or false if the mail is includes in the array or not
+ */
+function userInputInArray(userMail){
+  return mails.includes(userMail)
+}
+
+
+/**
+ * Add or remove the register button
+ */
+function buttonToggleVisibility(){
+  if(registerButton.classList.contains('d-none')){
+    registerButton.classList.remove("d-none");
+    registerButton.classList.add("d-block");
+  } else {
+    registerButton.classList.add("d-none");
+    registerButton.classList.remove("d-block");
+  }
+}
+
+// add error style in the input and in the result text
+function addErrorStyle(){
+  checkResult.classList.add("text-danger");
+  userInput.classList.add("input-error"); 
+}
+
+// remove error styles in the input and in the result text
+function removeErrorStyle(){
+  checkResult.classList.remove("text-danger");
+  userInput.classList.remove("input-error"); 
+}
+
+
